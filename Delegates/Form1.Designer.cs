62247d7f550224eb025d605b2c70ac0d664d1ad1
DaoUyen0808 <daouyen23102002@gmail.com>
2024-11-10 11:44:36 +0700

    add project

---
+++ Delegates/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Delegates
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumberOfButtons = new System.Windows.Forms.TextBox();
+            this.btnCreateButtons = new System.Windows.Forms.Button();
+            this.panelButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(40, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số lượng nút";
+            // 
+            // txtNumberOfButtons
+            // 
+            this.txtNumberOfButtons.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberOfButtons.Location = new System.Drawing.Point(44, 76);
+            this.txtNumberOfButtons.Name = "txtNumberOfButtons";
+            this.txtNumberOfButtons.Size = new System.Drawing.Size(197, 27);
+            this.txtNumberOfButtons.TabIndex = 1;
+            // 
+            // btnCreateButtons
+            // 
+            this.btnCreateButtons.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(133)))), ((int)(((byte)(163)))));
+            this.btnCreateButtons.ForeColor = System.Drawing.Color.White;
+            this.btnCreateButtons.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCreateButtons.Location = new System.Drawing.Point(265, 76);
+            this.btnCreateButtons.Name = "btnCreateButtons";
+            this.btnCreateButtons.Size = new System.Drawing.Size(75, 27);
+            this.btnCreateButtons.TabIndex = 2;
+            this.btnCreateButtons.Text = "Tạo nút";
+            this.btnCreateButtons.UseVisualStyleBackColor = false;
+            this.btnCreateButtons.Click += new System.EventHandler(this.btnCreateButtons_Click);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.AutoScroll = true;
+            this.panelButtons.Location = new System.Drawing.Point(44, 132);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(296, 181);
+            this.panelButtons.TabIndex = 3;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(382, 378);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.btnCreateButtons);
+            this.Controls.Add(this.txtNumberOfButtons);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form";
+            this.Text = "Tạo tự động số lượng nút";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumberOfButtons;
+        private System.Windows.Forms.Button btnCreateButtons;
+        private System.Windows.Forms.FlowLayoutPanel panelButtons;
+    }
+}
+
---
+++ Delegates/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegates
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        // Delegate để xử lý sự kiện click của nút
+        public delegate void ButtonClickHandler(object sender, EventArgs e);
+        public event ButtonClickHandler ButtonClicked;
+        public Form()
+        {
+            InitializeComponent();
+            // Đăng ký sự kiện ButtonClicked
+            ButtonClicked += OnButtonClicked;
+            // Xử lý sự kiện click của nút "Tạo nút"
+            btnCreateButtons.Click += btnCreateButtons_Click;
+
+        }
+
+        private void btnCreateButtons_Click(object sender, EventArgs e)
+        {
+            panelButtons.Controls.Clear();
+
+            // Kiểm tra số lượng nút hợp lệ
+            if (int.TryParse(txtNumberOfButtons.Text, out int numberOfButtons))
+            {
+                int yOffset = 0; // Khoảng cách dọc giữa các nút
+
+                for (int i = 1; i <= numberOfButtons; i++)
+                {
+                    Button newButton = new Button
+                    {
+                        Text = $"Nút {i}",
+                        Width = 80,
+                        Height = 30,
+                        Tag = i // Sử dụng Tag để lưu thông tin nút
+                    };
+
+                    // Đăng ký sự kiện click cho từng nút
+                    newButton.Click += (s, args) =>
+                    {
+                        ButtonClicked?.Invoke(s, args);
+                    };
+
+                    // Thiết lập vị trí của nút để không bị chồng lên nhau
+                    newButton.Location = new Point(10, yOffset); // Xác định tọa độ của nút trong panel
+                    yOffset += 40; // Tăng yOffset để các nút nằm cách nhau theo chiều dọc
+
+                    // Thêm nút vào panel
+                    panelButtons.Controls.Add(newButton);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập một số hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Phương thức xử lý sự kiện click cho các nút
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (sender is Button clickedButton)
+            {
+                MessageBox.Show($"Bạn đã nhấn {clickedButton.Text}", "Thông báo");
+            }
+        }
+    }
+}
